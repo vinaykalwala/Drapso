32728b18e1cf617bf37601179f7e38f6325fdb5e
LaxmiPrasanna-2593 <laxmiprasanna2593@gmail.com>
2026-03-30 13:03:24 +0530

    views ,urls created for general pages

--- Drapso/urls.py
+++ Drapso/urls.py
@@ -3,9 +3,14 @@ from django.urls import include, path
 from django.conf.urls.static import static
 
 from django.conf import settings
+from general import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- general/views.py
+++ general/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def privacy_policy(request):
+    return render(request, 'privacy_policy.html')
