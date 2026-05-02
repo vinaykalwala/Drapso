63052d42d4b2c912c5d98c380eade5c9e38cd451
LaxmiPrasanna-2593 <laxmiprasanna2593@gmail.com>
2026-05-02 16:03:17 +0530

    sidebar fixed

--- general/context_processors.py
+++ general/context_processors.py
@@ -1,6 +1,17 @@
 from django.conf import settings
 
 def global_settings(request):
-    return {
+    data = {
         "MAIN_DOMAIN": settings.MAIN_DOMAIN
     }
+
+    if request.user.is_authenticated and hasattr(request.user, "role"):
+        if request.user.role == "reseller":
+            from resellers.models import Store
+
+            # ✅ FIX HERE
+            store = Store.objects.filter(reseller=request.user).first()
+
+            data["store"] = store
+
+    return data
